0dafdcc1b41ddc3927178949b835c327638a4084
Muhammad Ahad <mohammad.ahad.321@gmail.com>
2023-05-23 17:55:44 +0200

    two number calculations

---
+++ TwoNumbers.py
@@ -0,0 +1,14 @@
+
+
+def sum(a, b): return a + b
+
+def multiply(x,y): return x*y
+
+def divide(x,y): return x/y
+
+def remainder(x ,y): return x%y
+class main():
+    print(sum(3,2))
+    print(multiply(2,3))
+    print(divide(6,3))
+    print(remainder(7,6))
